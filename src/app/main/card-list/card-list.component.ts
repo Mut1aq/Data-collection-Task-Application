60e79376f5f9953dadf92b37e617732e89dab848
Mutlaq <76498218+MutlaqFaharan@users.noreply.github.com>
2022-06-13 15:56:03 +0300

    Material Tabs working correctly

---
+++ src/app/main/card-list/card-list.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Survey } from 'src/app/models/survey.model';
+
+@Component({
+  selector: 'app-card-list',
+  templateUrl: './card-list.component.html',
+  styleUrls: ['./card-list.component.css'],
+})
+export class CardListComponent implements OnInit {
+  constructor(public http: HttpClient) {}
+  surveys: Survey[] = [];
+
+  ngOnInit(): void {
+    this.http
+      .get('https://mocki.io/v1/fec2e155-8904-4dc8-a97a-2b7015f474ed')
+      .subscribe(
+        (data) => {
+          this.surveys = data[0];
+          for (const survey of this.surveys) {
+            console.log(JSON.parse(survey['SurveyPeriods']), '*');
+          }
+        },
+        (err) => {
+          console.log(err);
+        }
+      );
+  }
+}
